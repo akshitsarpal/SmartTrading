2ce645a12a490fa316f33b29a03d13e0def40c05
sarpalak <asarpal.banks@gmail.com>
2020-09-05 23:44:32 -0700

    limit 5 requests er minute

--- data_extraction/constants.py
+++ data_extraction/constants.py
@@ -1,7 +1,8 @@
 import datetime as dt
 
 today = dt.date.today().isoformat()
-price_store_path = '/Users/akshit/SmartTrading_data/prices/{}'.format(today)
+price_store_path = '/Users/akshit/SmartTrading_data/prices/'
 
 default_days_intra = 14
 default_days_daily = 90
+sleep_time_sec = 70
--- data_extraction/extract_prices.py
+++ data_extraction/extract_prices.py
@@ -5,13 +5,13 @@ from string import digits
 import datetime as dt
 from alpha_vantage.timeseries import TimeSeries
 from multiprocessing import Pool, cpu_count
+import time
 
 from constants import *
 from args import parse_args
 from logger import logger
 
-# os.environ["ALPHAVANTAGE_API_KEY"] = "MY_API_KEY"
-
+os.environ["ALPHAVANTAGE_API_KEY"] = "100HED30C7HC9JMI"
 
 class Stock(object):
     '''
@@ -25,11 +25,11 @@ class Stock(object):
         self.stock_index = stock_index
         self.price_type = price_type
         self.ts = ts
-        self.prices_df = None
         self.after_hours = after_hours
         self.start_date = start_date
         self.save_csv = save_csv
         
+
     def _remove_digits(self, input_str):
         remove_digits = str.maketrans('', '', digits)
         res = input_str.translate(remove_digits)
@@ -38,13 +38,22 @@ class Stock(object):
 
     def _check_start_date_format(self):
         if not self.start_date:
-            self.start_date = pd.Timestamp(dt.date.today()-dt.timedelta(days=default_days_intra))
-            logger.info('''
-            {ticker}: "start_date" not specified, using default of {dy}
-            (business) days, with start date of {dt}.
-            '''.format(ticker=stock_ticker, dy=default_days_intra, 
-                       dt=self.start_date.isoformat()))
-            return
+            if self.price_type == 'intraday':
+                self.start_date = pd.Timestamp(dt.date.today()-dt.timedelta(days=default_days_intra))
+                logger.info('''
+                "start_date" not specified, using default of {dy}
+                (business) days, with start date of {dt}.
+                '''.format(dy=default_days_intra, 
+                           dt=self.start_date.isoformat()))
+                return
+            elif self.price_type == 'daily':
+                self.start_date = pd.Timestamp(dt.date.today()-dt.timedelta(days=default_days_daily))
+                logger.info('''
+                "start_date" not specified, using default of {dy}
+                (business) days, with start date of {dt}.
+                '''.format(dy=default_days_daily, 
+                           dt=self.start_date.isoformat()))
+                return
         elif isinstance(self.start_date, str):
             self.start_date = pd.Timestamp(self.start_date)
             return
@@ -69,7 +78,7 @@ class Stock(object):
             payload = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies', header=0)
             self.tickers_df = payload[0]
             if len(self.tickers_df.index) >= 450:
-                self.tickers_df = self.tickers_df.head(2) #TODO: remove this
+                self.tickers_df = self.tickers_df.head(6) #TODO: remove this
                 self.tickers_ds = self.tickers_df.rename({'Symbol':'Ticker'}, axis=1)
                 self.tickers_list = list(self.tickers_df['Symbol'])
             else:
@@ -82,7 +91,7 @@ class Stock(object):
             payload = pd.read_html('https://en.wikipedia.org/wiki/NASDAQ-100#Components', header=0)
             self.tickers_df = payload[3]
             if len(self.tickers_df.index) >= 90:
-                self.tickers_df = self.tickers_df.head(2) #TODO: remove this
+                self.tickers_df = self.tickers_df.head(6) #TODO: remove this
                 self.tickers_list = list(self.tickers_df['Ticker'])
             else:
                 ValueError('Check wikipedia data source for NASDAQ at \
@@ -98,7 +107,6 @@ class Stock(object):
         '''
         Extract prices using the API.
         '''
-        self._check_start_date_format()
         if self.price_type in ['intraday', 'daily']:
             try:
                 if self.price_type == 'intraday':
@@ -133,25 +141,41 @@ class Stock(object):
         '''
         Fetch stock price for list of tickers.
         '''
+        self._check_start_date_format()
         if not self.tickers_list:
             raise KeyError('tickers_list not provided, either pass as argument or \
                 call get_tickers_index() method with index name to fetch tickers.')
-        self.pool = Pool(cpu_count()-1)
-        prices_df_list = self.pool.map_async(self.get_prices_av, self.tickers_list).get()
-        self.prices_df = pd.concat(prices_df_list)
-        if self.save_csv:
-            try:
-                today = dt.date.today().strftime('%Y_%m_%d')
-                self.prices_df.to_csv(price_store_path+self.price_type+'_prices_'+today+'.csv', index=True)
-                logger.info('Saved prices.')
-            except:
-                logger.warning('Could not save prices.')
+        n_proc = max(cpu_count()-1, 4)
+        self.pool = Pool(n_proc)
+        
+        # Split into chunks of no more than 5 since Alpha Vantage API takes 5 calls per min 
+        tickers_list_chunked = [self.tickers_list[i:i+n_proc] 
+                                for i in range(0, len(self.tickers_list), n_proc)]
+        for chunk in tickers_list_chunked:
+            prices_df_list = self.pool.map_async(self.get_prices_av, chunk).get()
+            if self.save_csv:
+                for ticker_prices in prices_df_list:
+                    if len(ticker_prices.index) > 0:
+                        try:
+                            today = dt.date.today().strftime('%Y_%m_%d')
+                            print(type(ticker_prices))
+                            print(ticker_prices.shape)
+                            ticker_prices.to_csv(price_store_path+ticker_prices.index.levels[0][0]+'_'+
+                                                 self.price_type+'_'+today+'.csv', index=True)
+                            logger.info('{ticker}: Saved prices.'.format(ticker=ticker_prices.index.levels[0][0]))
+                        except:
+                            logger.warning('Could not save prices.')
+            logger.info("System sleep {} sec ....".format(sleep_time_sec))
+            time.sleep(sleep_time_sec)
+        self.pool.close()
+        self.pool.join() 
 
     
     def __getstate__(self):
         self_dict = self.__dict__.copy()
         del self_dict['pool']
         return self_dict
+
 
     def __setstate__(self, state):
         self.__dict__.update(state)
@@ -174,3 +198,4 @@ if __name__=='__main__':
               start_date=start_date, save_csv=save_csv)
     s.get_tickers_index()
     s.get_list_stock_prices()
+
