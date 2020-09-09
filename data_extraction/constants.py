1a33163b0ec0ed4248a23acafa7d99d8d843038c
sarpalak <asarpal.banks@gmail.com>
2020-09-09 01:14:13 -0700

    optimize sleep time, add constants

--- data_extraction/constants.py
+++ data_extraction/constants.py
@@ -8,5 +8,10 @@ today = dt.date.today().isoformat()
 price_store_path = '/Users/akshit/SmartTrading_data/prices/'
 
 default_days_intra = 14
-default_days_daily = 90
+default_days_daily = 10000
 sleep_time_sec = 70
+
+nasdaq_ll = 85
+nasdaq_ul = 130
+sp500_ll = 450
+sp500_ul = 600
