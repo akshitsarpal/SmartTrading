deca086d9e4b8ad4535c3700b38035a6dd2f69df
sarpalak <asarpal.banks@gmail.com>
2021-01-12 22:46:32 -0800

    change intraday threshold and minor fix

--- data_extraction/constants.py
+++ data_extraction/constants.py
@@ -7,7 +7,7 @@ sys.path.append(ST_HOME)
 today = dt.date.today().isoformat()
 PRICE_STORE_PATH = '/Users/akshit/SmartTrading_data/prices/{}/'.format(today)
 
-DEFAULT_DAYS_INTRA = 20 # trucate intraday prices older than 'default_days_intra' days
+DEFAULT_DAYS_INTRA = 30 # trucate intraday prices older than 'default_days_intra' days
 DEFAULT_DAYS_DAILY = 10000 # trucate daily prices older than 'default_days_intra' days
 SLEEP_TIME_SEC = 70 # sleep time between AlphaVantage API requests
 
