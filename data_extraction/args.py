9f0e099efe0081a8f67daf6fa0b3ce3461ab12de
sarpalak <asarpal.banks@gmail.com>
2021-04-08 17:31:38 -0700

    cleanup

--- data_extraction/args.py
+++ data_extraction/args.py
@@ -17,16 +17,14 @@ def parse_args():
     parser.add_argument('--start-date',
         type=str, required=False, default=None,
         help='Start date for stock prices to extract.')
-
     parser.add_argument('--earnings-bootstrap',
         action='store_true', required=False, default=False,
-        help='Set True to bootstrap historical earnings or False to save only new.')
-    
+        help='''Set True to bootstrap historical earnings or False to save 
+                only recent within last TRUNCATE_BUFFER days.''')
     parser.add_argument('--write-csv', 
         action='store_true', required=False, default=False,
         help='Whether to write prices to csvs.')
     parser.add_argument('--write-db', 
         action='store_true', required=False, default=False,
         help='Whether to write prices to db.')
-
     return parser.parse_args()
